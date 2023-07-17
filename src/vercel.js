ade9dd6ea1c693ec09d7f8ac94a2defed638b245
Alexander <alex1.polulyah@gmail.com>
2023-07-17 10:44:31 +0300

    hook2

--- src/vercel.js
+++ src/vercel.js
@@ -29,7 +29,7 @@ function startServer(bot) {
         console.log('req', req.body.host3)
         console.log('bot', bot)
         bot.setWebHook(req.body.host3)
-        res.status(200).json('Сервер hook работает!')
+        res.status(200).json(`Сервер hook работает! (${req.body.host3})`)
     });
     
     app.listen(3000, () => {
